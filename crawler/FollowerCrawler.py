90600710d4019967ad5b349b17e7ea2befd4afdd
birdben <191654006@163.com>
2017-04-26 12:10:38 +0800

    代码格式标准化修改

--- crawler/FollowerCrawler.py
+++ crawler/FollowerCrawler.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# coding=utf-8
+
 import random
 import threading
 import re
--- parser/BaseParser.py
+++ parser/BaseParser.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# coding=utf-8
+
 import threading
 from logger.LoggingRoot import rootLogger
 
